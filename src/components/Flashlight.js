b1ed60b60becf6f5b953114e4542d1ad47449f48
Roy Hwang <royhhwang@gmail.com>
2018-05-16 19:16:04 -0700

    flashlight

---
+++ src/components/Flashlight.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-materialize';
+import Lightswitch from '../img/lightswitch.png';
+import Parallax from 'parallax-js';
+import Eyes from '../img/eyes.png';
+import '../css/Flashlight.css';
+
+class Flashlight extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lights: false,
+            monster: true
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.parallax = new Parallax(this.scene);
+    }
+
+    componentWillUnmount() {
+        this.parallax.disable();
+    }
+
+    handleClick = () => {
+
+        this.setState({ lights: !this.state.lights });
+        this.setState({ monster: !this.state.monster });
+
+        if (this.state.lights === false) {
+            document.documentElement.style.setProperty('--lights', 0);
+        }
+        else {
+            document.documentElement.style.setProperty('--lights', 'radial-gradient( circle 15vmax at var(--cursorX) var(--cursorY), rgba(0, 0, 0, 0) 10%, rgba(0, 0, 0, 0.35) 90%, rgba(0, 0, 0, 0.99) 100%)');
+        }
+    };
+
+    render() {
+
+        const flipSwitch = this.state.lights ? 'flip-up' : 'flip-down';
+        const visibleMonster = this.state.monster ? 'visible-monster' : 'invisible-monster';
+
+        return (
+            <Row>
+                <Col className="s12">
+                    <div ref={el => this.scene = el} className="block">
+                        <div data-depth="0.2" id="media-monster">
+                            <img src={Eyes} alt="spooky monster" className={visibleMonster + ' monster-layer'}></img>
+                        </div>
+                        <div data-depth="0.4">
+                            <img src={Eyes} alt="smoke ring" className="smoke-layer"></img>
+                        </div>
+                    </div>
+                </Col>
+                <Col className="s12 light-switch">
+                    <h1 className="block1">Spooky</h1>
+                    <img src={Lightswitch} className={flipSwitch} onClick={this.handleClick} alt="lightswitch"></img>
+                </Col>
+            </Row>
+        )
+    }
+}
+
+export default Flashlight;
--- src/components/Front.js
+++ src/components/Front.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
-import Parallax from 'parallax-js';
-import Monster from '../img/eyes.png';
 import { Row, Col } from 'react-materialize';
+import Flashlight from './Flashlight';
+import Feed from './Feed';
 import '../css/Front.css';
 
 class Front extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { x: 0, y: 0 };
+        this.state = {
+            x: 0,
+            y: 0,
+        };
         this.onMouseMove = this.onMouseMove.bind(this);
-    }
-
-    componentDidMount() {
-        this.parallax = new Parallax(this.scene);
-    }
-    componentWillUnmount() {
-        this.parallax.disable();
     }
 
     onMouseMove = (e) => {
         this.setState({ x: e.screenX, y: e.screenY });
+
         const x = e.screenX || e.touches[0].screenX;
         const y = e.screenY || e.touches[0].screenY;
+
         document.documentElement.style.setProperty('--cursorX', x + 'px');
         document.documentElement.style.setProperty('--cursorY', y + 'px');
-
     }
 
     render() {
-
         return (
             <div>
                 <div className="mouse-coord" onMouseMove={this.onMouseMove.bind(this)} >
-                    <div ref={el => this.scene = el}>
-                        <div data-depth="0.2" id="media-monster">
-                            <img src={Monster} alt="spooky monster" className="monster-layer"></img>
-                        </div>
-                    </div>
                     <Row>
-                        <Col className="s12 block1">
-                            <h1>Spooky</h1>
-                            <h3>List of Spooky Games</h3>
-                            <p style={{ color: 'black' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis faucibus dui eget ornare. In hac habitasse platea dictumst. Curabitur ultrices vulputate leo ut convallis. Suspendisse porttitor lacus nec libero aliquet, vitae interdum nibh scelerisque. Donec convallis orci eu bibendum scelerisque. Duis non diam eros. Morbi vel lectus elementum, tincidunt nulla ultrices, pulvinar est. Nullam volutpat ex id venenatis semper. Aliquam tempus, urna eu consectetur ultricies, diam nisi iaculis nisi, eu pharetra dui tortor sed tellus. Nullam dictum dolor a lacus semper, eu rhoncus sem consectetur. Curabitur a tortor lorem. Mauris dignissim in tellus vitae aliquet. Cras ipsum augue, porta ullamcorper euismod nec, suscipit eu dui. Phasellus sagittis faucibus velit eget feugiat. Etiam aliquam elit erat, vitae placerat massa eleifend in. Donec sed tempus enim.</p>
+                        <Col className="s12 feed-block">
+                            <Flashlight />
+                            <Feed />
                         </Col>
                     </Row>
                 </div>
