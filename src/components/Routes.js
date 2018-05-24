f4b65a72c4d4045bbd612f79636861d8a9878723
Roy Hwang <royhhwang@gmail.com>
2018-05-23 20:45:16 -0700

    aframe setup

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, HashRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import Front from './Front';
 import AFrame from './AFrame';
 
 const Routes = () => (
-    <BrowserRouter>
+    <HashRouter>
         <div>
             <Route exact path="/" component={Front} />
             <Route exact path="/aframe" component={AFrame} />
         </div>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Routes;
